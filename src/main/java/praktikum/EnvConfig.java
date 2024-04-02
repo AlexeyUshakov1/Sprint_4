770f6f33154a1a92a2f7145e51121bf0d02e211c
CornCorn <47444275+AlexeyUshakov1@users.noreply.github.com>
2024-04-03 01:46:18 +0300

    Delete src directory

--- src/main/java/praktikum/EnvConfig.java
+++
@@ -1,8 +0,0 @@
-package praktikum;
-
-public class EnvConfig {
-    public static final String BASE_URL = "https://qa-scooter.praktikum-services.ru/";
-    public static final int DEFAULT_TIMEOUT = 10;
-    public static final String INVALID_TEXT = "ќшибка, отображаемый текст не совпадает с ожидаемым";
-
-}
