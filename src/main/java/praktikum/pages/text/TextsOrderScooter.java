a05d7450dc18ffe306553d00e41fa753a0187232
Corncorn <corncornlki@gmail.com>
2024-04-03 01:34:00 +0300

    2nd commit

---
+++ src/main/java/praktikum/pages/text/TextsOrderScooter.java
@@ -0,0 +1,10 @@
+package praktikum.pages.text;
+
+public class TextsOrderScooter {
+    public static final String NAME = "Степан";
+    public static final String LAST_NAME = "Степанов";
+    public static final String ADRES = "г. Вологда, ул. Ленина 1";
+    public static final String PHONE = "79059012345";
+    public static final String DATA = "10.04.2024";
+    public static final String COMMENT_TO_COURIER = "Позвонить за час?";
+}
